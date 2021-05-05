e4a4fd64769436d7c806acac8635a1e43587b49d
Arch-Storm <42775469+Arch-Storm@users.noreply.github.com>
2021-05-05 08:27:09 +0000

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+import { App, Modal, Notice, Plugin } from 'obsidian';
+
+export default class MyPlugin extends Plugin {
+	async onload() {
+		this.addCommand({
+			id: 'obsidian-indent-lines',
+			name: 'Indentation Lines',
+		});
+	}
+}
