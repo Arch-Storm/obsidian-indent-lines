c363d82f7cb745ee4472c948c28078cbd2aff82d
Arch-Storm <42775469+Arch-Storm@users.noreply.github.com>
2021-05-06 16:53:44 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,2 @@
 import { Plugin } from 'obsidian';
-
-export default class MyPlugin extends Plugin {
-	async onload() {
-		this.addCommand({
-			id: 'obsidian-indent-lines',
-			name: 'Indentation Lines',
-		});
-	}
-}
+export default class MyPlugin extends Plugin {}
